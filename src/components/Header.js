f9d7ca0366edf66c65a9c59c914baf79fa997765
AbdulRehman2236 <abdulrehmanfayyaz361@gmail.com>
2024-08-30 12:37:18 +0500

    adjust menu bar wrt medium size screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,13 @@ const Header = () => {
   return (
     <header className="bg-white">
       {isMenuClose ? (
-        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 md:px-2  lg:px-8" aria-label="Global">
+          <div className="flex md:flex-1">
             <a href="/" className="-m-1.5 p-1.5">
               <h1 className="h8 w-auto font-bold text-xl">COLLECTION</h1>
             </a>
           </div>
-          <div className="flex lg:hidden">
+          <div className="flex md:hidden">
             <div className="flex flex-1 justify-end">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
@@ -46,7 +46,7 @@ const Header = () => {
               </svg>
             </button>
           </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+          <div className="hidden md:flex md:gap-x-10 lg:flex lg:gap-x-12">
             <div className="relative">
               <Link className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" to="/">
                 HOME
@@ -79,7 +79,7 @@ const Header = () => {
               </button>
             </div>
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <div className="hidden md:flex md:flex-1 md:justify-end">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
               <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
             </svg>
