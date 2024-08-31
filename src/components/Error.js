5f3b8895d99b4f080cf138b732335c5e0b4ba0b0
AbdulRehman2236 <abdulrehmanfayyaz361@gmail.com>
2024-08-31 22:36:39 +0500

    Adjust error page bg

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,21 +5,21 @@ const Error = () => {
   const error = useRouteError();
 
   return (
-    <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
+    <section className="flex items-center min-h-[100vh] p-16 bg-gray-800 text-gray-50 ">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
-          <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
+          <h2 className="mb-8 font-extrabold text-9xl">
             <span className="sr-only">Error</span>
             {error.status ? error.status : 404}
           </h2>
           <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-          <p className="mt-4 mb-8 dark:text-gray-600">
-            But dont worry, you can find plenty of other things on our homepage.
+          <p className="mt-4 mb-8 text-gray-100">
+            But do not worry, you can find plenty of other things on our homepage.
           </p>
           <Link
             rel="noopener noreferrer"
             to="/"
-            className="px-8 py-3 font-semibold rounded text-white bg-black hover:bg-white hover:text-black hover:border border-black hover:text-sm hover:font-bold"
+            className="px-8 py-3 font-semibold rounded text-white bg-gray-700 hover:bg-white border border-black hover:text-black hover:border border-black hover:text-sm hover:font-bold"
           >
             Back to Home
           </Link>
