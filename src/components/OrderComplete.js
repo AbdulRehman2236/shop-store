d7f89d4b98cdae4287bf4a42c6094f15bd559c71
AbdulRehman2236 <abdulrehmanfayyaz361@gmail.com>
2024-09-02 00:18:25 +0500

    Added order complete page

--- src/components/OrderComplete.js
+++ src/components/OrderComplete.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const OrderComplete = () => {
-  return <div>OrderComplete</div>;
+  return (
+    <section className="flex items-center h-full sm:p-16 dark:bg-gray-800 dark:text-gray-50">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-28 text-green-400"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+        <p className="text-3xl font-semibold">Thank you for your order!</p>
+        <p className="mt-4 mb-8 dark:text-gray-100">
+          Your order has been dispatched, and will arrive just as fast as the pony can get there!
+        </p>
+        <Link
+          rel="noopener noreferrer"
+          to="/"
+          className="px-8 py-3 font-semibold rounded bg-black text-white hover:bg-white hover:text-black hover:border border-black dark:bg-gray-100 dark:text-black dark:hover:bg-gray-500 dark:hover:text-gray-50"
+        >
+          Back to Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default OrderComplete;
